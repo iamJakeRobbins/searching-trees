b1ea861f3f9e49d5613fc5808d5a90cbf7176efc
Jake Robbins <jrobbinsd@gmail.com>
2018-01-12 13:09:57 -0700

    initial commit

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -33,14 +33,42 @@ class BinarySearchTree {
 	}
 
 	dfs(cb, node=this.root) {
+		cb(node.data)
+	if(node.left){
+		this.dfs(cb, node.left)
+	}
+	if(node.right){
+		this.dfs(cb, node.right)
+	}
+	 }
+
+
     // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
-	}
+
 
 	bfs(cb, node=this.root) {
-    // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
-    // Expected order: 25, 15, 197, 17, 97, 9997
+		const q = []
+		q.push(node)
+			while (q.length > 0) {
+				let current = q.shift()
+				cb(current.data)
+				if (current.left) q.push(current.left)
+				if (current.right) q.push(current.right)
+			}
+  //
+	// 	// Brooks code is below, if uses methods from the Queue.js file---
+	// 	// 	const nodes = new Queue()
+	// 	// 	nodes.enqueue(node)
+	// 	// while (!nodes.isEmpty) {
+	// 	// 	node = nodes.dequeue()
+	// 	// 	cb(node.data);
+	// 	// 	if (node.left) nodes.enqueue(node.left);
+	// 	// 	if (node.right) nodes.enqueue(node.right)
+  //   //
+	// 	// }
 	}
 }
+
 
 module.exports = BinarySearchTree
